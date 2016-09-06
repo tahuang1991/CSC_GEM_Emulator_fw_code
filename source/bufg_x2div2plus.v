369e668acbd4778482a37f7b558d12b67dd7ba93
Andrew Peck <anp@ucla.edu>
2016-09-06 15:48:27 -0700

    Change source of gtx usr clocks from TXOUTCLK to QPLL 40MHz

--- source/bufg_x2div2plus.v
+++ source/bufg_x2div2plus.v
@@ -66,7 +66,13 @@
 
 `timescale 1ps/1ps
 
+`define fortymhz
+
+`ifdef fortymhz
+(* CORE_GENERATION_INFO = "bufg_x2div2,clk_wiz_v1_8,{component_name=bufg_x2div2,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,feedback_source=FDBK_AUTO,primtype_sel=MMCM_ADV,num_out_clk=3,clkin1_period=25.0,clkin2_period=10.0,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,use_status=false,use_freeze=false,use_clk_valid=false,feedback_type=SINGLE,clock_mgr_type=MANUAL,manual_override=false}" *)
+`else
 (* CORE_GENERATION_INFO = "bufg_x2div2,clk_wiz_v1_8,{component_name=bufg_x2div2,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,feedback_source=FDBK_AUTO,primtype_sel=MMCM_ADV,num_out_clk=3,clkin1_period=12.5,clkin2_period=10.0,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,use_status=false,use_freeze=false,use_clk_valid=false,feedback_type=SINGLE,clock_mgr_type=MANUAL,manual_override=false}" *)
+`endif
 module bufg_x2div2plus
  (// Clock in ports
   input         CLK_IN1,
@@ -115,23 +121,35 @@ module bufg_x2div2plus
     .CLOCK_HOLD           ("FALSE"),
     .COMPENSATION         ("ZHOLD"),
     .STARTUP_WAIT         ("FALSE"),
+
     .DIVCLK_DIVIDE        (1),
-    .CLKFBOUT_MULT_F      (12.000),
+
+    `ifdef fortymhz .CLKFBOUT_MULT_F  (24.0),
+    `else           .CLKFBOUT_MULT_F  (12.0),
+    `endif
+
     .CLKFBOUT_PHASE       (0.000),
     .CLKFBOUT_USE_FINE_PS ("FALSE"),
+
+    `ifdef fortymhz .CLKIN1_PERIOD  (25.0),
+    `else           .CLKIN1_PERIOD  (12.5),
+    `endif
+
     .CLKOUT0_DIVIDE_F     (12.000),
     .CLKOUT0_PHASE        (0.000),
     .CLKOUT0_DUTY_CYCLE   (0.500),
     .CLKOUT0_USE_FINE_PS  ("FALSE"),
+
     .CLKOUT1_DIVIDE       (6),
     .CLKOUT1_PHASE        (0.000),
     .CLKOUT1_DUTY_CYCLE   (0.500),
     .CLKOUT1_USE_FINE_PS  ("FALSE"),
+
     .CLKOUT2_DIVIDE       (24),
     .CLKOUT2_PHASE        (0.000),
     .CLKOUT2_DUTY_CYCLE   (0.500),
     .CLKOUT2_USE_FINE_PS  ("FALSE"),
-    .CLKIN1_PERIOD        (12.5),
+
     .REF_JITTER1          (0.010))
   mmcm_adv_inst
     // Output clocks
